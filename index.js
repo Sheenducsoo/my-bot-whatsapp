8f3a808a693872084cdc10c1af92bdc93b669d9c
Sheenducsoo <127421914+Sheenducsoo@users.noreply.github.com>
2025-06-23 22:32:33 -0400

    Actulaizar Puppertear

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 app.use(express.json());
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
 
 client.on('qr', qr => {
